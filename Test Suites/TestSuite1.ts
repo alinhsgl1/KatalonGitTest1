bd4ca8e35a1e3d8a56b29e7b26469abc21ce20b8
alinhsgl <alinhsgl@gmail.com>
2019-09-14 01:39:57 +0300

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>df7f9536-5e6a-4bbf-9c6c-68e6c4f99526</testSuiteGuid>
+</TestSuiteEntity>
